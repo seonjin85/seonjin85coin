90c316f717354d13d447338525fc004cca19fcd0
SeonJin jang <seonjin85@gmail.com>
2021-11-28 22:48:07 +0900

    #1-Blockchain-5-Refactoring-part-two

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/seonjin85/seonjin85coin/blockchain"
+import (
+	"fmt"
+
+	"github.com/seonjin85/seonjin85coin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data : %s\n", block.Data)
+		fmt.Printf("Hata : %s\n", block.Hash)
+		fmt.Printf("Prev Hata : %s\n", block.PrevHash)
+	}
 }
