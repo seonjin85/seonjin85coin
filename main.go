cc1be7c4eb15ba200b911061db4842cb77e2ce8b
SeonJin jang <seonjin85@gmail.com>
2021-12-02 15:59:54 +0900

    #2-Explorer-1-Rendering-Templates

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
+
+	"github.com/seonjin85/seonjin85coin/blockchain"
 )
 
 const port string = ":4000"
 
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "hello from home!!")
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
 }
 
 func main() {
