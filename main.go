7d366520a671402d929e9f0cea07662bdac493ff
SeonJin jang <seonjin85@gmail.com>
2021-12-05 23:35:48 +0900

    #3-RESTAPI-0-Setup

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"github.com/seonjin85/seonjin85coin/explorer"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/seonjin85/seonjin85coin/utils"
 )
 
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documemtation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
+
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documemtation)
+	fmt.Printf("Listening on http:localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
