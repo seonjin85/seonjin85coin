cd479395e6c0a161bf1651f80e768bb5d937aca2
SeonJin jang <seonjin85@gmail.com>
2021-12-10 23:53:21 +0900

    #3-RESTAPI-8-Middlewares

--- rest/rest.go
+++ rest/rest.go
@@ -78,7 +78,6 @@ func documemtation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See A Block",
 		},
 	}
-	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 
@@ -95,12 +94,21 @@ func block(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(block)
 }
 
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
+}
+
 func Start(aPort int) {
 	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
+	router.Use(jsonContentTypeMiddleware)
 	router.HandleFunc("/", documemtation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET")
+
 	fmt.Printf("Listening on http:localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
