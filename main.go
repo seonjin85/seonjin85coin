e1f3d8084fa41427a4581085f9a6c70a293fb111
SeonJin jang <seonjin85@gmail.com>
2021-12-31 15:48:47 +0900

    #4-CLI-2-FlagSet

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func usage() {
-	fmt.Printf("Wecome to seonjin85 coin\n")
-	fmt.Printf("Please use the following commands:\n")
+	fmt.Printf("Wecome to seonjin85 coin\n\n")
+	fmt.Printf("Please use the following commands:\n\n")
 	fmt.Printf("explorer:    Start the HTML Explorer\n")
 	fmt.Printf("rest:        Start the REST API (recommended)\n")
 	os.Exit(0)
@@ -18,12 +19,20 @@ func main() {
 		usage()
 	}
 
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
 	switch os.Args[1] {
 	case "explorer":
 		fmt.Println("Start Explorer")
 	case "rest":
-		fmt.Println("Start REST API")
+		rest.Parse(os.Args[2:])
 	default:
 		usage()
+	}
+
+	if rest.Parsed() {
+		fmt.Println(portFlag)
+		fmt.Println("Start server")
 	}
 }
