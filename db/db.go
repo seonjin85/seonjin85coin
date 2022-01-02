2ecc23df2e27f082c5ede8084d4a2798133fcffb
SeonJin jang <seonjin85@gmail.com>
2022-01-02 16:10:23 +0900

    #5-Persistence-1-Creating-The-Database

---
+++ db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"github.com/boltdb/bolt"
+	"github.com/seonjin85/seonjin85coin/utils"
+)
+
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		db = dbPointer
+		utils.HandleErr(err)
+
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utils.HandleErr(err)
+	}
+
+	return db
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/seonjin85/seonjin85coin
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+)
