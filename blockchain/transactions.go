186d7cce8224c6a3df14415a2bbf909c44f4e107
SeonJin jang <seonjin85@gmail.com>
2022-02-05 00:06:45 +0900

    #7-Transactions-3-AddTx

--- blockchain/transactions.go
+++ blockchain/transactions.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"errors"
 	"time"
 
 	"github.com/seonjin85/seonjin85coin/utils"
@@ -9,6 +10,12 @@ import (
 const (
 	minerReward int = 50
 )
+
+type mempool struct {
+	TXs []*Tx
+}
+
+var Mempool *mempool = &mempool{}
 
 type Tx struct {
 	Id        string   `json:"id"`
@@ -48,4 +55,20 @@ func makeCoinbaseTx(address string) *Tx {
 	}
 	tx.getID()
 	return &tx
+}
+
+func makeTx(from, to string , amount, int) (*TX, error){
+  if Blockchain().BalanceByAddress(from) < amount{
+	  return nil, errors.New("not enough money")
+  }
+}
+
+func (m *mempool) AddTx(to string, amount int) error {
+	tx , err := makeTx("seonjin85",to, amount)
+
+	if err != nil{
+		return err
+	}
+	m.TXs = append(m.TXs, tx)
+	return nil
 }
--- rest/rest.go
+++ rest/rest.go
@@ -124,6 +124,10 @@ func balance(rw http.ResponseWriter, r *http.Request) {
 
 }
 
+func mempool(rw http.ResponseWriter, r *http.Request) {
+	utils.HandleErr(json.NewEncoder(rw).Encode(blockchain.Mempool.TXs))
+}
+
 func Start(aPort int) {
 	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
@@ -133,6 +137,7 @@ func Start(aPort int) {
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{hash:[a-f0-9]+}", block).Methods("GET")
 	router.HandleFunc("/balance/{address}", balance).Methods("GET")
+	router.HandleFunc("/mempool", mempool)
 	fmt.Printf("Listening on http:localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
