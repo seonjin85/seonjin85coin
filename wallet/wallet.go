4eb20d2683196481c7f33f531751d84f09134283
SeonJin jang <seonjin85@gmail.com>
2022-02-15 23:49:50 +0900

    #8-Wallet-6-Persist-Wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,7 +2,12 @@ package wallet
 
 import (
 	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
 	"os"
+
+	"github.com/seonjin85/seonjin85coin/utils"
 )
 
 // 1) we hash the msg.
@@ -17,6 +22,10 @@ import (
 // 4) verify
 // ("hashed_message" + "signature" + publick) -> true / false
 
+const (
+	fileName string = "seonjin85.wallet"
+)
+
 type wallet struct {
 	privateKey *ecdsa.PrivateKey
 }
@@ -24,15 +33,32 @@ type wallet struct {
 var w *wallet
 
 func hasWalletFile() bool {
-	_, err := os.Stat("seonjin85.wallet")
+	_, err := os.Stat(fileName)
 	return !os.IsNotExist(err)
+}
+
+func createPrivKey() *ecdsa.PrivateKey {
+	privKey, err := ecdsa.GenerateKey(elliptic.P224(), rand.Reader)
+	utils.HandleErr(err)
+	return privKey
+}
+
+func persistKey(key *ecdsa.PrivateKey) {
+	bytes, err := x509.MarshalECPrivateKey(key)
+	utils.HandleErr(err)
+	err = os.WriteFile(fileName, bytes, 0644)
+	utils.HandleErr(err)
 }
 
 func Wallet() *wallet {
 	if w == nil {
-
+		w = &wallet{}
 		if hasWalletFile() {
 
+		} else {
+			key := createPrivKey()
+			persistKey(key)
+			w.privateKey = key
 		}
 	}
 	return w
