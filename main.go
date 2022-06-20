85b3e2602e541303cad2466c14fcd59a06446fb2
SeonJin jang <seonjin85@gmail.com>
2022-06-20 16:18:39 +0900

    #9-P2P-4-Buffered-Channels

--- main.go
+++ main.go
@@ -5,30 +5,32 @@ import (
 	"time"
 )
 
-func countToTen(c chan<- int) {
+func send(c chan<- int) {
 	for i := range [10]int{} {
+		fmt.Printf(">> sending %d<<\n", i)
 		c <- i
-		time.Sleep(1 * time.Second)
+		fmt.Printf(">> sent %d <<\n", i)
 	}
 	close(c)
 }
 
 func receive(c <-chan int) {
 	for {
+		time.Sleep(10 * time.Second)
 		a, ok := <-c
 		if !ok {
 			fmt.Println("we are done")
 			break
 		}
-		fmt.Printf("received %d\n", a)
+		fmt.Printf("|| received %d ||\n", a)
 	}
 }
 
 func main() {
 	// defer db.Close()
 	// cli.Start()
-	c := make(chan int)
-	go countToTen(c)
+	c := make(chan int, 10)
+	go send(c)
 	receive(c)
 
 }
