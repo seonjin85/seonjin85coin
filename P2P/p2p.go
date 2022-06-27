81676657d1288c58c4680e9d0463e7b4b27ee9d2
SeonJin jang <seonjin85@gmail.com>
2022-06-27 14:26:42 +0900

    #9-P2P-12-Recap

--- P2P/p2p.go
+++ P2P/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/websocket"
 	"github.com/seonjin85/seonjin85coin/utils"
@@ -13,20 +12,22 @@ var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	// port :3000 will upgared the request from :4000
-	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	openPort := r.URL.Query().Get("openPort")
+	ip := utils.Splitter(r.RemoteAddr, ":", 0)
+
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return openPort != "" && ip != ""
+	}
 
 	// http conn --> websocket conn
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-
-	openPort := r.URL.Query().Get("openPort")
-	result := strings.Split(r.RemoteAddr, ":")
-	initPeer(conn, result[0], openPort)
+	initPeer(conn, ip, openPort)
 }
 
 func AddPeer(address, port, openPort string) {
 	// from :4000 is requesting an upgrade from the port :3000
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
 }
