ef61f69713903a9e6a83b0f67bf35d4dabc1b445
SeonJin jang <seonjin85@gmail.com>
2022-06-27 15:14:28 +0900

    #9-P2P-14-Inbox

--- P2P/p2p.go
+++ P2P/p2p.go
@@ -23,16 +23,16 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	// http conn --> websocket conn
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	initPeer(conn, ip, openPort)
+	peer := initPeer(conn, ip, openPort)
 	time.Sleep(20 * time.Second)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from port 3000!"))
+	peer.inbox <- []byte("Hello from 3000!")
 }
 
 func AddPeer(address, port, openPort string) {
 	// from :4000 is requesting an upgrade from the port :3000
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	initPeer(conn, address, port)
+	peer := initPeer(conn, address, port)
 	time.Sleep(10 * time.Second)
-	conn.WriteMessage(websocket.TextMessage, []byte("Hello from port 4000!"))
+	peer.inbox <- []byte("Hello from 4000")
 }
