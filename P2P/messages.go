4381eff99e34f45cf5f960c919172f60de123b05
SeonJin jang <seonjin85@gmail.com>
2022-06-28 16:39:15 +0900

    #9-P2P-20-Newest Block

--- P2P/messages.go
+++ P2P/messages.go
@@ -1,5 +1,12 @@
 package p2p
 
+import (
+	"encoding/json"
+
+	"github.com/seonjin85/seonjin85coin/blockchain"
+	"github.com/seonjin85/seonjin85coin/utils"
+)
+
 type MessageKind int
 
 const (
@@ -13,3 +20,26 @@ type Message struct {
 	Payload []byte
 }
 
+func (m *Message) addPayload(p interface{}) {
+	//  marshal : json -> v , unmarshal : v -> json
+	b, err := json.Marshal(p)
+	utils.HandleErr(err)
+	m.Payload = b
+}
+
+func makeMessage(kind MessageKind, payload interface{}) []byte {
+	m := Message{
+		Kind: kind,
+	}
+	m.addPayload(payload)
+	mJson, err := json.Marshal(m)
+	utils.HandleErr(err)
+	return mJson
+}
+
+func sendNewstBlock(p *peer) {
+	b, err := blockchain.FindBlock(blockchain.Blockchain().NewestHash)
+	utils.HandleErr(err)
+	m := makeMessage(MessageNewstBlock, b)
+	p.inbox <- m
+}
