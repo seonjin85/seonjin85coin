f903d7e40f184b7b89f61a9fdf42409daa0fca2b
SeonJin jang <seonjin85@gmail.com>
2022-08-08 15:36:29 +0900

    #9-P2P-25-Recap

--- P2P/p2p.go
+++ P2P/p2p.go
@@ -19,6 +19,7 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 		return openPort != "" && ip != ""
 	}
 
+	fmt.Printf("%s wants an upgrade \n", openPort)
 	// http conn --> websocket conn
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
@@ -27,6 +28,7 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 
 func AddPeer(address, port, openPort string) {
 	// from :4000 is requesting an upgrade from the port :3000
+	fmt.Printf("%s want to connect to port %s\n", openPort, port)
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	p := initPeer(conn, address, port)
