f10dd7d110bea121dfd020f19ca7a38297d4180a
SeonJin jang <seonjin85@gmail.com>
2022-08-19 16:02:43 +0900

    #10-P2P-5-Full-Converage

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"reflect"
@@ -81,5 +82,33 @@ func TestHandleErr(t *testing.T) {
 
 	if !called {
 		t.Error("HandleError should call fn")
+	}
+}
+
+func TestFromBytes(t *testing.T) {
+	type testStruct struct {
+		Test string
+	}
+	var restored testStruct
+	ts := testStruct{"test"}
+	b := ToBytes(ts)
+	FromBytes(&restored, b)
+	if !reflect.DeepEqual(ts, restored) {
+		t.Error("FromBytes() should restore struct")
+	}
+}
+
+func TestToJSON(t *testing.T) {
+	type testStruct struct{ Test string }
+	s := testStruct{"test"}
+	b := ToJSON(s)
+	k := reflect.TypeOf(b).Kind()
+	if k != reflect.Slice {
+		t.Errorf("Expected %v and got %v", reflect.Slice, k)
+	}
+	var restored testStruct
+	json.Unmarshal(b, &restored)
+	if !reflect.DeepEqual(s, restored) {
+		t.Error("TestToJSON() should encode to JSON correclty.")
 	}
 }
